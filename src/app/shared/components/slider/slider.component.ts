9caec3f8ac7bdced36b7338d334ee9d90c991a0d
Marcos Lucas <marcosvaldeni@gmail.com>
2024-07-06 10:21:05 +0100

    how the Data in Frontend

--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-slider',
@@ -8,13 +8,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SliderComponent implements OnInit {
 
+  movies: any;
+
   constructor(
     private httpClient: HttpClient
   ) { }
 
   ngOnInit(): void {
-    this.httpClient.get('https://api.themoviedb.org/3//movie/popular?api_key=5cdffd6344124ea865d0973fa852792a')
-      .subscribe((data) => console.log)
+    this.getPupularMovies();
+  }
+
+  getPupularMovies() {
+    const header = new HttpHeaders({ 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Y2RmZmQ2MzQ0MTI0ZWE4NjVkMDk3M2ZhODUyNzkyYSIsIm5iZiI6MTcxOTY1NTI1MC45ODc4NTcsInN1YiI6IjY0MTIyMzM5ZmU2YzE4MDBmOWJkNGVmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XPy9ngOOeAlRLbsyvL1Tr0ukNWbxhjAkiNlOqU1_330'});
+    this.httpClient.get('https://api.themoviedb.org/3//movie/popular', { headers: header})
+      .subscribe((data) => {
+        this.movies = data;
+      });
   }
 
 
